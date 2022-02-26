b9436a3aa7037554be3d4af5ed30c60779c1f9dd
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-02-26 23:20:47 +1000

    rev: new logic

--- server/api/AVAR.routes.ts
+++ server/api/AVAR.routes.ts
@@ -1,17 +1,19 @@
 import express from 'express'
 
-import AVARSchema from '../models/AVAR.model'
+import AVARModel from '../models/AVAR.model'
 
 const AVARRouter = express.Router()
 
 AVARRouter.get('/', (req, res) => {
-  AVARSchema.findOne({ MON: 1 }, (error: any, data: any) => {
+  let limit = Number(req.query.limit)
+
+  AVARModel.find((error: any, data: any) => {
     if (error) {
-      console.log(`An error occured: ${error.res}`)
+      console.log(`An error occured: ${error}`)
     } else {
       res.json(data)
     }
-  })
+  }).limit(limit)
 })
 
 export default AVARRouter
