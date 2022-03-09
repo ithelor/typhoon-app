68373f2397aa449cc0528b647eae8b21813029f8
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-03-09 23:09:57 +1000

    fix sidebar provider children

--- client/src/layouts/SidebarProvider.tsx
+++ client/src/layouts/SidebarProvider.tsx
@@ -3,13 +3,17 @@ import React from 'react'
 import { SidebarContext } from 'contexts/SidebarContext'
 
 interface ISidebarProvider {
-  children: React.ReactElement
+  children: React.ReactElement | React.ReactElement[]
 }
 
 const SidebarProvider = (props: ISidebarProvider) => {
   const [sidebarOpen, setSidebarOpen] = React.useState(false)
 
-  return <SidebarContext.Provider value={{ sidebarOpen, setSidebarOpen }}>{props.children}</SidebarContext.Provider>
+  return (
+    <SidebarContext.Provider value={{ sidebarOpen, setSidebarOpen }}>
+      {props.children}
+    </SidebarContext.Provider>
+  )
 }
 
 export default SidebarProvider
