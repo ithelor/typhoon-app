709f5055f02b6260101816cacaec26692c26ce42
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-03-10 00:13:09 +1000

    feat: adaptive-ish mobile layout

--- client/src/layouts/Header/Header.tsx
+++ client/src/layouts/Header/Header.tsx
@@ -1,17 +1,22 @@
 import { FaBars as HamburgerIcon } from 'react-icons/fa'
 
 import { useSidebar } from 'hooks/useSidebar'
+import { useBackdrop } from 'hooks/useBackdrop'
 
 import styles from './Header.module.scss'
 
 const Header = () => {
   const { sidebarOpen, setSidebarOpen } = useSidebar()
+  const { backdropOpen, setBackdropOpen } = useBackdrop()
 
   return (
     <header className={styles.container}>
       <button
         className={styles.sidebarToggle}
-        onClick={() => setSidebarOpen(!sidebarOpen)}
+        onClick={() => {
+          setSidebarOpen(!sidebarOpen)
+          setBackdropOpen(!backdropOpen)
+        }}
       >
         <HamburgerIcon className={styles.toggleIcon} />
       </button>
--- client/src/layouts/Sidebar/Sidebar.tsx
+++ client/src/layouts/Sidebar/Sidebar.tsx
@@ -46,34 +46,41 @@ const Sidebar = () => {
   const { sidebarOpen } = useSidebar()
 
   return (
-    <aside
-      className={classNames(styles.container, { [styles.open]: sidebarOpen })}
-    >
-      <div className={styles.topSection}>
-        <div className={styles.logo}>
-          <Logo />
+    <>
+      <div
+        className={classNames(styles.placeholder, {
+          [styles.open]: sidebarOpen
+        })}
+      />
+      <aside
+        className={classNames(styles.container, { [styles.open]: sidebarOpen })}
+      >
+        <div className={styles.topSection}>
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+          <div className={classNames(styles.hiddenSidebar, styles.title)}>
+            Title
+          </div>
+          <div className={classNames(styles.hiddenSidebar, styles.subtitle)}>
+            Subtitle
+          </div>
         </div>
-        <div className={classNames(styles.hiddenSidebar, styles.title)}>
-          Title
+        <div className={styles.middleSection}>
+          <ul className={styles.menuList}>
+            <MenuItem title="Dashboard" to="#" icon={DashboardIcon} active />
+            <MenuItem title="Content" to="#" icon={ContentIcon} />
+            <MenuItem title="Analytics" to="#" icon={AnalyticsIcon} />
+          </ul>
         </div>
-        <div className={classNames(styles.hiddenSidebar, styles.subtitle)}>
-          Subtitle
+        <div className={styles.bottomSection}>
+          <ul className={styles.menuList}>
+            <MenuItem title="Profile" to="#" icon={ProfileIcon} />
+            <MenuItem title="Settings" to="#" icon={SettingsIcon} />
+          </ul>
         </div>
-      </div>
-      <div className={styles.middleSection}>
-        <ul className={styles.menuList}>
-          <MenuItem title="Dashboard" to="#" icon={DashboardIcon} active />
-          <MenuItem title="Content" to="#" icon={ContentIcon} />
-          <MenuItem title="Analytics" to="#" icon={AnalyticsIcon} />
-        </ul>
-      </div>
-      <div className={styles.bottomSection}>
-        <ul className={styles.menuList}>
-          <MenuItem title="Profile" to="#" icon={ProfileIcon} />
-          <MenuItem title="Settings" to="#" icon={SettingsIcon} />
-        </ul>
-      </div>
-    </aside>
+      </aside>
+    </>
   )
 }
 
