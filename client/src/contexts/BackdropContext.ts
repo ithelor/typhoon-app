9daf36e7fe0210be67485a608dbd6fe61ff85e9d
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-03-17 23:27:58 +1000

    remove deprecated code

--- client/src/contexts/BackdropContext.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export const BackdropContext = React.createContext<any>(null)
--- client/src/hooks/useBackdrop.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-import { BackdropContext } from 'contexts/BackdropContext'
-
-export const useBackdrop = () => React.useContext(BackdropContext)
