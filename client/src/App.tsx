855eb8c83b49ea27ade38e85ada5abbc09e371a8
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-03-17 23:26:26 +1000

    refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,18 +12,18 @@ const App = () => (
       <SidebarProvider>
         <Header />
         <Sidebar />
-        <main className="content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet error obcaecati adipisci
-          praesentium minus! Ut, itaque commodi molestias temporibus quo sint voluptate quisquam
-          expedita labore deleniti earum repellat architecto, nulla minima rem hic vero obcaecati
-          quasi? Nulla, placeat consectetur. Laudantium nihil, ad rem illum quae expedita asperiores
-          aliquam quas repudiandae! Totam maiores temporibus laudantium nihil quos pariatur eum
-          veritatis, nam sint adipisci mollitia nisi ducimus itaque alias in sit consequuntur
-          deleniti molestiae recusandae. Sequi rem ipsum temporibus officia fuga ratione at
-          asperiores odio id laborum repellat, porro voluptatum molestiae nulla voluptates, cum
-          dicta quod inventore hic reprehenderit nisi! Repudiandae, veniam.
-        </main>
       </SidebarProvider>
+      <main className="content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet error obcaecati adipisci
+        praesentium minus! Ut, itaque commodi molestias temporibus quo sint voluptate quisquam
+        expedita labore deleniti earum repellat architecto, nulla minima rem hic vero obcaecati
+        quasi? Nulla, placeat consectetur. Laudantium nihil, ad rem illum quae expedita asperiores
+        aliquam quas repudiandae! Totam maiores temporibus laudantium nihil quos pariatur eum
+        veritatis, nam sint adipisci mollitia nisi ducimus itaque alias in sit consequuntur deleniti
+        molestiae recusandae. Sequi rem ipsum temporibus officia fuga ratione at asperiores odio id
+        laborum repellat, porro voluptatum molestiae nulla voluptates, cum dicta quod inventore hic
+        reprehenderit nisi! Repudiandae, veniam.
+      </main>
     </div>
   </BrowserRouter>
 )
