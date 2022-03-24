7361ba9d030e3c34db883c7b1e13f589c5c553d6
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-03-24 23:46:28 +1000

    ci: custom head prop

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -5,36 +5,41 @@ import './Table.module.scss'
  */
 interface ITable {
   caption?: string
+  head?: string[]
   data: {}[]
 }
 
 // TODO: custom no data message ?
 // TODO: fill if not enough to render tds ?
-const Table = (props: ITable) => (
-  <table>
-    {props.caption && <caption>{props.caption}</caption>}
-    <thead>
-      <tr>
-        {props.data.length > 0 ? (
-          Object.keys(props.data[0]).map((item, key) => <th key={key}>{item}</th>)
-        ) : (
-          <th>
-            <h3>Nothing to display</h3>
-          </th>
-        )}
-      </tr>
-    </thead>
+const Table = (props: ITable) => {
+  const headData =
+    props.head?.map((item, key) => <th key={key}>{item}</th>) ||
+    Object.keys(props.data[0]).map((item, key) => <th key={key}>{item}</th>)
 
-    <tbody>
-      {props.data.map((item: Object, key) => (
-        <tr key={key}>
-          {Object.values(item).map((item, key) => (
-            <td key={key}>{item}</td>
-          ))}
+  return (
+    <table>
+      {props.caption && <caption>{props.caption}</caption>}
+      <thead>
+        <tr>
+          {headData || (
+            <th>
+              <h3>Nothing to display</h3>
+            </th>
+          )}
         </tr>
-      ))}
-    </tbody>
-  </table>
-)
+      </thead>
+
+      <tbody>
+        {props.data.map((item: Object, key) => (
+          <tr key={key}>
+            {Object.values(item).map((item, key) => (
+              <td key={key}>{item}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
 export default Table
