1dabe480abeb84f50d5cf1e90d4666bd29456418
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-03-28 20:12:33 +1000

    ci: share interfaces

--- client/src/interfaces/index.ts
+++
@@ -1,5 +0,0 @@
-import IContreg from 'interfaces/Contreg.interface'
-import INPunkt from 'interfaces/NPunkt.interface'
-import IRegion from 'interfaces/Region.interface'
-
-export type { IContreg, INPunkt, IRegion }
---
+++ shared/interfaces/index.ts
@@ -0,0 +1,5 @@
+import IContreg from './Contreg.interface';
+import INPunkt from './NPunkt.interface';
+import IRegion from './Region.interface';
+
+export type { IContreg, INPunkt, IRegion };
