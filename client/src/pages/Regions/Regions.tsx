c300e18ffc5c1ae7ce8e509fc40b3b4937d5aace
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-04-01 02:20:41 +1000

    ci: add api data fetching

--- client/src/pages/Regions/Regions.tsx
+++ client/src/pages/Regions/Regions.tsx
@@ -4,124 +4,90 @@ import { useSearchParams } from 'react-router-dom'
 import Block from 'components/Block'
 import Loader from 'components/Loader'
 
-import { IContreg, INpunkt, IRegion } from '@shared/interfaces'
+import { IRegion } from '@shared/interfaces'
 
-import { getRegions } from 'api/services/regions'
-import { get } from 'api/services'
+import { getRegions, getRegion, getAdjacent, getCenter } from 'api/services/regions'
 
 import './Regions.module.scss'
 
-// loaded from api on first render
-interface IStaticData {
-  contregs: IContreg[]
-  npunkts: INPunkt[]
-  regions: IRegion[]
-}
+/**
+ * Regions page
+ * TODO: ? reducer (state { static {...}, current, dynamic {...} })
+ *
+ * TODO: move auxiliaries to helpers
+ * TODO: fill-in animation
+ */
 
-// recalculated on 'currentRegion' update
 interface IDynamicData {
-  adjacent: string[]
+  adjacent: string | string[]
   center: string
 }
 
-/**
- * Regions page
- * TODO: move data processing to the server
- * TODO: reducer (state { static {...}, current, dynamic {...} }) ?
- *
- * TODO: per-block loading, 'cause too long
- * TODO: move auxiliaries to helpers
- */
 const Regions = () => {
   const [searchParams, setSearchParams] = useSearchParams(),
-    paramCurrent = searchParams.get('current')
+    paramCurrent = searchParams.get('current') ?? 'VVPR'
 
-  const [staticData, setstaticData] = React.useState<IStaticData>({
-    contregs: [],
-    npunkts: [],
-    regions: []
-  })
+  // loaded once on first render
+  const [loading, setLoading] = React.useState(true)
+  const [regions, setRegions] = React.useState<IRegion[]>([])
+
+  // either found by search params or taken first from regions
   const [currentRegion, setCurrentRegion] = React.useState({} as IRegion)
+
+  // updated on currentRegion update
+  const [fetching, setFetching] = React.useState(false)
   const [dynamicData, setDynamicData] = React.useState<IDynamicData>({
     adjacent: [],
     center: ''
   })
 
-  const [loading, setLoading] = React.useState(true)
+  React.useEffect(() => {
+    const fetchStatic = async () => {
+      setRegions(
+        (await getRegions()).data.sort((a, b) => (a.Name < b.Name ? -1 : a.Name > b.Name ? 1 : 0))
+      )
+    }
+
+    fetchStatic()
+  }, [])
 
   React.useEffect(() => {
-    ;(async () => {
-      let contregsResponse = await get('contreg'),
-        npunktResponse = await get('npunkt'),
-        regionsResponse = await getRegions()
+    const fetchDynamic = async () => {
+      const region = (await getRegion(paramCurrent)).data
 
-      Promise.all([regionsResponse, contregsResponse, npunktResponse])
-        .then(() => {
-          ;(regionsResponse.data as IRegion[]).sort((a: IRegion, b: IRegion) =>
-            a.Name < b.Name ? -1 : a.Name > b.Name ? 1 : 0
-          )
+      try {
+        const [adjacentData, centerData] = await Promise.all([
+          getAdjacent(region.KOD),
+          getCenter(region.KOD)
+        ])
 
-          setstaticData({
-            contregs: contregsResponse.data,
-            npunkts: npunktResponse.data,
-            regions: regionsResponse.data
-          })
+        setCurrentRegion(region)
+        setDynamicData({ adjacent: adjacentData.data, center: centerData.data })
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setLoading(false)
+        setFetching(false)
+      }
+    }
 
-          const getFirstNotEmpty = (regionsResponse.data as IRegion[]).find(
-            (region) => region.KOD && region.Name
-          )
-
-          const getRegionByCode = regionsResponse.data.find(
-            (region: IRegion) => region.KOD === paramCurrent
-          )
-
-          setCurrentRegion(paramCurrent ? getRegionByCode : getFirstNotEmpty)
-        })
-        .then(() => setLoading(false))
-    })()
+    fetchDynamic()
   }, [searchParams]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  React.useEffect(() => {
-    setDynamicData({
-      adjacent: (() => {
-        let result: string[] = []
-
-        staticData.regions.forEach((region) =>
-          staticData.contregs.forEach(
-            (contreg) =>
-              contreg.KOD1 === currentRegion.KOD &&
-              contreg.KOD2 === region.KOD &&
-              result.push(region.Name)
-          )
-        )
-        return result
-      })(),
-      center: (() =>
-        staticData.npunkts.find((npunkt) => npunkt.KOD === currentRegion.Center)?.NAME!)()
-    })
-
-    paramCurrent &&
-      setCurrentRegion(staticData.regions.find((region) => region.KOD === paramCurrent)!)
-  }, [searchParams, currentRegion]) // eslint-disable-line react-hooks/exhaustive-deps
-
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFetching(true)
     setSearchParams({ current: event.target.value })
-    setDynamicData((prev) => ({
-      ...prev,
-      adjacent: []
-    }))
   }
 
-  // TODO: fill-in animation
   return (
     <section>
       {loading ? (
         <Loader />
       ) : (
         <>
-          <select onChange={handleSelectChange} value={paramCurrent || undefined}>
-            {staticData.regions.map(
-              (region, index) =>
+          <select onChange={handleSelectChange} value={paramCurrent}>
+            {regions.map(
+              (region) =>
                 region.KOD &&
                 region.Name && (
                   <option key={region.KOD} value={region.KOD}>
@@ -131,11 +97,17 @@ const Regions = () => {
             )}
           </select>
 
-          <Block heading="Прилегающие муниципальные образования" data={dynamicData.adjacent} />
-          <Block heading="Центр" data={dynamicData.center} />
-          <Block heading="Глава Муниципального образования" data={currentRegion.Remark} />
-          <Block heading="Общая площадь территории" data={currentRegion.STER} />
-          <Block heading="Численность населения" data={currentRegion.POPULATION} />
+          {fetching ? (
+            <Loader />
+          ) : (
+            <>
+              <Block heading="Прилегающие муниципальные образования" data={dynamicData.adjacent} />
+              <Block heading="Центр" data={dynamicData.center} />
+              <Block heading="Глава Муниципального образования" data={currentRegion.Remark} />
+              <Block heading="Общая площадь территории" data={currentRegion.STER} />
+              <Block heading="Численность населения" data={currentRegion.POPULATION} />
+            </>
+          )}
         </>
       )}
     </section>
