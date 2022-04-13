c40d4b0f8c36b57383cd1f776e97b9e45889c750
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-04-13 23:56:53 +1000

    ci: update services with new endpoints

--- client/src/api/services/regions.ts
+++ client/src/api/services/regions.ts
@@ -1,10 +1,33 @@
 import API from 'api/api.instance'
 
-import { IRegion } from '@shared/interfaces'
+import { IDivision, IKomiss, INpunkt, IObjekt, IRegion, ISpop } from '@shared/interfaces'
 
 const getRegions = () => API.get<IRegion[]>(`regions`)
-const getRegion = (id: string) => API.get<IRegion>(`regions/${id}`)
-const getAdjacent = (id: string) => API.get<string[]>(`regions/${id}/adjacent`)
-const getCenter = (id: string) => API.get<string>(`regions/${id}/center`)
 
-export { getRegions, getRegion, getAdjacent, getCenter }
+const getRegion = (code: string) => API.get<IRegion>(`regions/${code}`)
+
+const getAdjacent = (code: string) => API.get<string[]>(`regions/${code}/adjacent`)
+
+const getRivers = (code: string) => API.get<string[]>(`regions/${code}/rivers`)
+
+const getKomiss = (code: string) => API.get<IKomiss[]>(`regions/${code}/komiss`)
+
+const getSpop = (code: string) => API.get<ISpop[]>(`regions/${code}/spop`)
+
+const getNpunkts = (code: string) => API.get<INpunkt[]>(`regions/${code}/npunkts`)
+
+const getObjekts = (code: string) => API.get<IObjekt[]>(`regions/${code}/objekts`)
+
+const getDivisions = (code: string) => API.get<IDivision[]>(`regions/${code}/divisions`)
+
+export {
+  getRegions,
+  getRegion,
+  getAdjacent,
+  getRivers,
+  getKomiss,
+  getSpop,
+  getNpunkts,
+  getObjekts,
+  getDivisions
+}
