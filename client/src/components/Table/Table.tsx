b7dfac0268e4ba107ef79c737a67c4e34d101392
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-05-16 23:58:04 +1000

    ci: update data handling

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -6,44 +6,55 @@ import styles from './Table.module.scss'
 interface ITable {
   caption?: string
   head?: string[]
-  data: {}[]
+  data: Object[]
 }
 
-// TODO: custom no data message ?
-// TODO: fill if not enough to render tds ?
-const Table = (props: ITable) => {
-  const headData =
-    props.head?.map((item, key) => <th key={key}>{item}</th>) ||
-    Object.keys(props.data[0]).map((item, key) => <th key={key}>{item}</th>)
+const handleHead = (head: ITable['head'], data: ITable['data']) => {
+  // if "head" is not being provided
+  // thead is being composed of "data" keys
 
   return (
-    <>
-      <h3 className={styles.caption}>{props.caption}</h3>
-      <div className={styles.wrapper}>
-        <table>
-          <thead>
-            <tr>
-              {headData || (
-                <th>
-                  <h3>Nothing to display</h3>
-                </th>
-              )}
-            </tr>
-          </thead>
-
-          <tbody>
-            {props.data.map((item: Object, key) => (
-              <tr key={key}>
-                {Object.values(item).map((item, key) => (
-                  <td key={key}>{item}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+    <tr>
+      {(head || Object.keys(data[0])).map((item, key) => (
+        <th key={key}>{item}</th>
+      ))}
+    </tr>
   )
 }
+
+const handleData = (data: ITable['data']) => {
+  // besides numbers or strings "data" may contain objects
+  // due to mongoose populations (usually only one property).
+  // if so, the property value is being returned.
+  // if not, the original value is being returned.
+
+  return data.map((item, index) => (
+    <tr key={index}>
+      {Object.values(item).map((value: number | string | Object, key) => {
+        // some mongoose populations result in null
+        // due to db values referring to invalid keys.
+        // because of that checking for null.
+
+        return (
+          <td key={key}>
+            {typeof value === 'object' && value !== null ? Object.values(value)[0] : value ?? '-'}
+          </td>
+        )
+      })}
+    </tr>
+  ))
+}
+
+const Table = (props: ITable) => (
+  <>
+    <h3 className={styles.caption}>{props.caption}</h3>
+    <div className={styles.wrapper}>
+      <table>
+        <thead>{handleHead(props.head, props.data)}</thead>
+        <tbody>{handleData(props.data)}</tbody>
+      </table>
+    </div>
+  </>
+)
 
 export default Table
