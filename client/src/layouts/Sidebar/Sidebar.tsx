7f228d883f6e64fd103dd808b27827ca15bb6b76
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-07-02 21:47:40 +1000

    ci: add icons

--- client/src/layouts/Sidebar/Sidebar.tsx
+++ client/src/layouts/Sidebar/Sidebar.tsx
@@ -1,4 +1,18 @@
 import classNames from 'classnames'
+import {
+  BsInfoCircle as AboutIcon,
+  BsLayoutTextWindow as RegionsIcon,
+  BsExclamationTriangle as DangerIcon,
+  BsPeople as CommitteeIcon,
+  BsCheck2All as LegalIcon,
+  BsFileText as DocsIcon,
+  BsPerson as PersonIcon,
+  BsFileCheck as SynopsisIcon,
+  BsShieldCheck as UnitsIcon,
+  BsClipboard as SituationIcon,
+  BsClipboardData as SummaryIcon,
+  BsArchive as ArchiveIcon
+} from 'react-icons/bs'
 
 import NavItem from 'layouts/Sidebar/components/NavItem'
 import Accordion from './components/Accordion'
@@ -13,29 +27,35 @@ const Sidebar = () => {
   return (
     <aside className={classNames(styles.container, { [styles.open]: sidebarContext?.isOpen })}>
       <nav className={styles.navMenu}>
-        <NavItem title="О системе" to="/about" />
-        <NavItem title="Паспорт Приморского Края" to="/regions" />
-        <NavItem title="Угроза ЧС" to="/outlook" />
-        <NavItem title="Краевая комиссия по ЧС" to="/committee" />
+        <NavItem title="О системе" to="/about" icon={<AboutIcon />} />
+        <NavItem title="Паспорт Приморского Края" to="/regions" icon={<RegionsIcon />} />
+        <NavItem title="Угроза ЧС" to="/outlook" icon={<DangerIcon />} />
+        <NavItem title="Краевая комиссия по ЧС" to="/committee" icon={<CommitteeIcon />} />
         <Accordion title="Нормативные акты">
-          <NavItem title="Законы РФ" to="/legal?type=ZRF" />
-          <NavItem title="Указы президента РФ" to="/legal?type=UPR" />
-          <NavItem title="Постановления и распоряжения правительства РФ" to="/legal?type=PPR" />
-          <NavItem title="Законы Приморского края" to="/legal?type=ZPK" />
+          <NavItem title="Все" to="/legal" icon={<LegalIcon />} />
+          <NavItem title="Законы РФ" to="/legal?type=ZRF" icon={<DocsIcon />} />
+          <NavItem title="Указы президента РФ" to="/legal?type=UPR" icon={<PersonIcon />} />
+          <NavItem
+            title="Постановления и распоряжения правительства РФ"
+            to="/legal?type=PPR"
+            icon={<DocsIcon />}
+          />
+          <NavItem title="Законы Приморского края" to="/legal?type=ZPK" icon={<DocsIcon />} />
           <NavItem
             title="Постановления и Распоряжения Губернатора (Администрации) Приморского края"
             to="/legal?type=PG"
+            icon={<PersonIcon />}
           />
-          <NavItem title="Документы МЧС" to="/legal?type=VD" />
-          <NavItem title="Документы ГО ЧС" to="/legal?type=DGC" />
-          <NavItem title="Документы других ведомств" to="/legal?type=VDO" />
+          <NavItem title="Документы МЧС" to="/legal?type=VD" icon={<DocsIcon />} />
+          <NavItem title="Документы ГО ЧС" to="/legal?type=DGC" icon={<DocsIcon />} />
+          <NavItem title="Документы других ведомств" to="/legal?type=VDO" icon={<DocsIcon />} />
         </Accordion>
-        <NavItem title="Критерии информации о ЧС" to="/synopsis" />
-        <NavItem title="Особо опасные объекты края" to="/targets" />
-        <NavItem title="Силы и средства защиты" to="/units" />
-        <NavItem title="Оперативная обстановка" to="/situation" />
-        <NavItem title="Сводка оперативной обстановки" to="/summary" />
-        <NavItem title="Архив ЧС, снятых с контроля" to="/archive" />
+        <NavItem title="Критерии информации о ЧС" to="/synopsis" icon={<SynopsisIcon />} />
+        <NavItem title="Особо опасные объекты края" to="/targets" icon={<DangerIcon />} />
+        <NavItem title="Силы и средства защиты" to="/units" icon={<UnitsIcon />} />
+        <NavItem title="Оперативная обстановка" to="/situation" icon={<SituationIcon />} />
+        <NavItem title="Сводка оперативной обстановки" to="/summary" icon={<SummaryIcon />} />
+        <NavItem title="Архив ЧС, снятых с контроля" to="/archive" icon={<ArchiveIcon />} />
       </nav>
     </aside>
   )
